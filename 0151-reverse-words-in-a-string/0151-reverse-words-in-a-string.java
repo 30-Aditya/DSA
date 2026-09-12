6f30bc9a2fce13875351b01f978c9793a1fc9b7a
30-Aditya <251432520+30-Aditya@users.noreply.github.com>
2026-09-13 00:07:19 +0530

    Time: 10 ms (17.7%), Space: 46.6 MB (17.63%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution { 
+    public String reverseWords(String s) { 
+        String[] sentence=s.trim().split("\\s+"); 
+        String result=""; 
+        for(int i=sentence.length-1;i>=0;i--){ 
+            result+=sentence[i]+" "; 
+        } 
+        return result.trim(); 
+    } 
+}
